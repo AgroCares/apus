5cdf8cd1bccf284658a8a3e033455a6279d4fc7b
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-04-03 15:01:22 +0200

    Add function to add fields

--- R/apus.R
+++ R/apus.R
@@ -8,6 +8,8 @@
 #'
 #' @importFrom R6 R6Class
 #' @import checkmate
+#' @import cli
+#' @import data.table
 #'
 #' @export
 Apus <- R6::R6Class(
@@ -17,11 +19,14 @@ Apus <- R6::R6Class(
     #' @field farm_name (character) Name of the farm
     farm_name = NULL,
 
+    #'@field fields (data.table) A table with the fields and their properties
+    fields = NULL,
 
     #' @description
     #' Creates a new instance of this [R6][R6::R6Class] class.
     #'
     #' @param farm_name (character) The name of the farm
+    #' @export
     initialize = function(farm_name) {
 
       # Check arguments ------------------------------------------------------
@@ -30,6 +35,55 @@ Apus <- R6::R6Class(
 
       # Assign farm name to object ----------------------------------------------
       self$farm_name <- farm_name
+
+      return(TRUE)
+    },
+
+    #' @description
+    #' Add a field to the apus object
+    #'
+    #' @param b_id (character) ID or (unique) name of the field
+    #' @param b_area (number) The area of the field (m^2)
+    #' @param b_lu (character) The cultivation code for this field
+    #' @param d_n_req (number) The required amount of Nitrogen for this field (kg N / ha)
+    #' @param d_p_req (number) The required amount of Phosphate for this field (kg P2O5 / ha)
+    #' @param d_k_req (number) The required amount of Potassium for this field (kg K2O / ha)
+    #' @param d_n_norm (number) The legal limit for workable Nitrogen (kg N / ha)
+    #' @param d_n_norm_man (number) The legal limit for total Nitrogen from manure (kg N / ha
+    #' @param d_p_norm (number) The legal limit for Phosphate (kg P2O5 / ha)
+    #'
+    #' @export
+    addField = function(b_id, b_area, b_lu, d_n_req = NA, d_p_req = NA, d_k_req = NA, d_n_norm = NA, d_n_norm_man = NA, d_p_norm = NA) {
+
+      # Check arguments ---------------------------------------------------------
+      # TODO
+
+      # Create table with the data ----------------------------------------------
+      field <- data.table(
+        b_id = b_id,
+        b_area = b_area,
+        b_lu = b_lu,
+        d_n_req = d_n_req,
+        d_p_req = d_p_req,
+        d_k_req = d_k_req,
+        d_n_norm = d_n_norm,
+        d_n_norm_man = d_n_norm_man,
+        d_p_norm = d_p_norm
+      )
+
+      # Append field to fields --------------------------------------------------
+      if (length(self$fields) == 0) {
+        setkey(field, b_id)
+        self$fields <- field
+      } else {
+
+        # Check if b_id is not already used ----------------------------------------
+        if (b_id %in% self$fields$b_id) {
+          cli_abort('The field {b_id} is already present and duplicate fields are not allowed.')
+        }
+
+        self$fields <- rbindlist(list(self$fields, field))
+      }
 
       return(TRUE)
     }
--- tests/testthat/test-apus.R
+++ tests/testthat/test-apus.R
@@ -1,8 +1,91 @@
+
+
+# Create apus object ------------------------------------------------------
+farm_name <- 'my_farm'
+apus <- apus::Apus$new(farm_name = farm_name)
+
 test_that("Apus is created", {
-
-  farm_name <- 'my_farm'
-  apus <- apus::Apus$new(farm_name = farm_name)
-
   expect_contains(class(apus), 'Apus')
   expect_equal(apus$farm_name, farm_name)
 })
+
+
+# Add first field to apus -------------------------------------------------
+field1 <- data.frame(
+  b_id = 'field_1',
+  b_area = 10000,
+  b_lu = 'nl_2014',
+  d_n_req = 270,
+  d_p_req = 120,
+  d_k_req = 50,
+  d_n_norm = 230,
+  d_n_norm_man = 170,
+  d_p_norm = 75
+)
+
+apus$addField(
+  b_id = field1$b_id,
+  b_area = field1$b_area,
+  b_lu = field1$b_lu,
+  d_n_req = field1$d_n_req,
+  d_p_req = field1$d_p_req,
+  d_k_req = field1$d_k_req,
+  d_n_norm = field1$d_n_norm,
+  d_n_norm_man = field1$d_n_norm_man,
+  d_p_norm = field1$d_p_norm
+)
+
+test_that("First field is added", {
+  expect_contains(class(apus$fields), 'data.table')
+  expect_equal(nrow(apus$fields), 1)
+  expect_equal(apus$fields$b_id[1], field1$b_id)
+  expect_equal(apus$fields$b_area[1], field1$b_area)
+  expect_equal(apus$fields$b_lu[1], field1$b_lu)
+  expect_equal(apus$fields$d_n_req[1], field1$d_n_req)
+  expect_equal(apus$fields$d_p_req[1], field1$d_p_req)
+  expect_equal(apus$fields$d_k_req[1], field1$d_k_req)
+  expect_equal(apus$fields$d_n_norm[1], field1$d_n_norm)
+  expect_equal(apus$fields$d_n_norm_man[1], field1$d_n_norm_man)
+  expect_equal(apus$fields$d_p_norm[1], field1$d_p_norm)
+})
+
+# Add second field --------------------------------------------------------
+field2 <- data.frame(
+  b_id = 'field_2',
+  b_area = 10000,
+  b_lu = 'nl_265',
+  d_n_req = 270,
+  d_p_req = 120,
+  d_k_req = 50,
+  d_n_norm = 230,
+  d_n_norm_man = 170,
+  d_p_norm = 75
+)
+
+apus$addField(
+  b_id = field2$b_id,
+  b_area = field2$b_area,
+  b_lu = field2$b_lu,
+  d_n_req = field2$d_n_req,
+  d_p_req = field2$d_p_req,
+  d_k_req = field2$d_k_req,
+  d_n_norm = field2$d_n_norm,
+  d_n_norm_man = field2$d_n_norm_man,
+  d_p_norm = field2$d_p_norm
+)
+
+test_that("Second field is added", {
+  expect_contains(class(apus$fields), 'data.table')
+  expect_equal(nrow(apus$fields), 2)
+  expect_equal(apus$fields$b_id[2], field2$b_id)
+  expect_equal(apus$fields$b_area[2], field2$b_area)
+  expect_equal(apus$fields$b_lu[2], field2$b_lu)
+  expect_equal(apus$fields$d_n_req[2], field2$d_n_req)
+  expect_equal(apus$fields$d_p_req[2], field2$d_p_req)
+  expect_equal(apus$fields$d_k_req[2], field2$d_k_req)
+  expect_equal(apus$fields$d_n_norm[2], field2$d_n_norm)
+  expect_equal(apus$fields$d_n_norm_man[2], field2$d_n_norm_man)
+  expect_equal(apus$fields$d_p_norm[2], field2$d_p_norm)
+})
+
+
