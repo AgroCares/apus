51383fc1e41c1155e382e07c8dd8f3770f13c711
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-04-04 12:01:06 +0200

    Set max values explicit

--- data-raw/parameters.R
+++ data-raw/parameters.R
@@ -7,9 +7,15 @@ library(data.table)
 # Preprocess dataset ------------------------------------------------------
 
 parameters <- pandex::nmi_parameters
-parameters <- parameters[code %in% c('B_ID', 'B_AREA', 'B_LU', 'D_N_REQ', 'D_P_REQ', 'D_K_REQ', 'D_N_NORM', 'D_N_NOR_MAN', 'D_P_NORM')]
+parameters <- parameters[code %in% c('B_ID', 'B_AREA', 'B_LU', 'D_N_REQ', 'D_P_REQ', 'D_K_REQ', 'D_N_NORM', 'D_N_NORM_MAN', 'D_P_NORM')]
 parameters[, code := tolower(code)]
 
+
+# Set min max explicit ----------------------------------------------------
+parameters[code == 'b_area', value_max := 20000]
+parameters[code == 'd_k_req', value_max := 100]
+parameters[code == 'd_n_req', value_max := 500]
+parameters[code == 'd_p_req', value_max := 200]
 
 # Export table ------------------------------------------------------------
 usethis::use_data(parameters, overwrite = TRUE, version = 3, compress = 'xz')
