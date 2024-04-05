c5f80c2d8e93361aa16703a171436ca39e9a8db9
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-04-05 15:14:09 +0200

    Add definition of a simple model

--- R/model.R
+++ R/model.R
@@ -3,7 +3,9 @@
 #' @description
 #' Creates a torch model to be used for apus
 #'
-#' @param farms (data.table)
+#' @param fields (data.table)
+#' @param fertilizers (data.table)
+#' @param
 #' @param device (character)
 #'
 #' @import checkmate
@@ -12,25 +14,30 @@
 #' @import torch
 #'
 #'@export
-createApusModel <- function(device) {
+createApusModel <- function(fields, fertilizers, width, depth, device) {
 
   # Check arguments ---------------------------------------------------------
   # TODO
 
   # Define torch dataset ----------------------------------------------------
   apus_model <- torch::nn(
-    name = "apus_dataset",
+    name = "apus_model",
 
-    initialize = function(dim_start, width, dim_end, depth, device) {
+    initialize = function(fields, fertilizers, width, depth, device) {
 
       # Check arguments -----------------------------------------------------
       # TODO
 
+      self$fields_count <- ncol(fields)
+      self$fertilizers_count <- ncol(fertilizers)
+
 
       # Setup the layers --------------------------------------------------------
-      self$fc_in <- torch::nn_linear(in_features = dim_start, out_features = width)
+      self$fc_in_fields <- torch::nn_linear(in_features = self$fields_count, out_features = width)
+      self$fc_in_fertilizers <- torch::nn_linear(in_features = self$fertilizers_count, out_features = width)
+      self$fc_cat <- torch::nn_linear(in_features = 2 * width, out_features = width)
       self$fc <- torch::nn_linear(width, width)
-      self$fc_out <- torch::nn_linear(in_features = width, out_features = dim_end)
+      self$fc_out <- torch::nn_linear(in_features = width, out_features = self$fertilizers_count * self$fields_count)
 
       self$activation <- torch::nn_prelu()
       self$activation_end <- torch::nn_relu()
@@ -38,19 +45,37 @@ createApusModel <- function(device) {
 
     },
 
-    forward = function (farms) {
+    forward = function (fields, fertilizers) {
 
-      x <- self$fc_in(farms)
-      y <- self$fc(x)
-      z <- self$fc_end(y)
+      x.fields <- self$fc_in_fields(fields)
+      x.fields <- self$activation(x.fields)
 
-      return(z)
+      x.fertilizers <- self$fc_in_fertilizers(fertilizers)
+      x.fertilizers <- self$activation(x.fertilizers)
+
+      x <- torch::torch_cat(list(x.fields, x.fertilizers))
+      y <- self$fc_cat(x)
+      y <- self$activation(y)
+
+      z <- self$fc(y)
+      z <- self$activation(z)
+
+      a <- self$fc_out(z)
+      a <- self$activation_end(a)
+      a <- torch::torch_reshape(a, list(self$fields_count, self$fertilizers_count))
+
+      return(a)
     }
   )
 
   # Create torch dataset for apus -------------------------------------------
-  model <- apus_model()
+  model <- apus_model(fields, fertilizers, width, depth, device)
 
-  return(dataset)
+
+  # Train the model ---------------------------------------------------------
+
+
+
+  return(model)
 }
 
