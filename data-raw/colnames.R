d23ce734688ac963484eb699c428a5a6d1eb7303
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-05-17 17:10:14 +0200

    Update colnames of fertilizers

--- data-raw/colnames.R
+++ data-raw/colnames.R
@@ -1,7 +1,7 @@
 
 # Set colnames for fertilizers --------------------------------------------
 
-cols.fertilizers <- c('p_stored', 'p_price', 'p_n_rt', 'p_n_wc', 'p_p_rt', 'p_k_rt')
+cols.fertilizers <- c('p_stored', 'p_price', 'p_n_rt', 'p_n_wc', 'p_p_rt', 'p_k_rt', 'p_type_manure', 'p_p_wcl')
 usethis::use_data(cols.fertilizers, overwrite = TRUE, version = 3)
 
 
